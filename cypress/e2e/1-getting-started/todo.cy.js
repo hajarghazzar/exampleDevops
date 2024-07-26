ba11c28ed6f528e5a60c31752aff8426b4a6f164
hajarghazzar <hajarghazzar@hotmail.fr>
2024-07-26 16:05:43 +0200

    ouiouiouitfuyg

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -5,7 +5,7 @@ Coucou
 // This spec file contains a variety of sample tests
 // for a todo list app that are designed to demonstrate
 // the power of writing tests in Cypress.
-//
+//cc
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
